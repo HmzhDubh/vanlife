7ddd22a0291122a176d3d8c8edb7edf68024afdc
HmzhDubh <meezo.2015@hotmail.com>
2025-03-20 07:14:47 +0300

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import { Link } from "react-router-dom"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import Home from "../components/Home"
+import Header from "./Header"
+import Footer from "./Footer"
+import Home from "./Home"
 
-import '../App.css'
+import './App.css'
 
 function App() {
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './index.css'
-import App from './components/App.jsx'
-import About from './components/about'
+import App from './App.jsx'
+import About from './about'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
