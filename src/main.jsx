8a276dcc30f6243cad16a383f0f0108d00040341
HmzhDubh <meezo.2015@hotmail.com>
2025-03-20 07:09:03 +0300

    fix path bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './index.css'
-import App from './components/App.jsx'
-import About from './components/about'
+import App from '/src/components/App.jsx'
+import About from '/src/components/about'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
